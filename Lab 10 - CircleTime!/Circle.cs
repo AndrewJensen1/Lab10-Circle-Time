f9e41afde1add86794b5c9c2196b0ba19515dea0
Andrew <Jensen1.Andrew@gmail.com>
2019-07-26 08:33:14 -0400

    first commit

---
+++ Lab 10 - CircleTime!/Circle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Lab_10___CircleTime_
+{
+    class Circle
+    {
+        //field start with a lower case camelCase
+        private double _radius;
+
+        //property are Capitalized PascalCase
+        public double Radius
+        {
+            get
+            {
+                return  _radius;
+            }
+            set
+            {
+                _radius = value;
+            }
+        }
+
+        //Constructor
+        public Circle (double radius)
+        {
+            _radius = radius;
+        }
+
+
+        //methods
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * _radius;
+        }
+        public double CalculateArea()
+        {
+           return Math.PI * Math.Pow(_radius, 2);
+            
+        }
+        public string CalculateFormatedArea()
+        {
+            double result = Math.Round(CalculateArea(), 2);
+            return result.ToString();
+        }
+        public string  CalculateFormatedCircumference()
+        {
+            double result = Math.Round(CalculateCircumference(), 2);
+            return result.ToString();
+        }
+    }
+}
---
+++ Lab 10 - CircleTime!/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Lab_10___CircleTime_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            bool go = true;
+            while (go)
+            {
+                Console.WriteLine("Welcome to the Circle Tester would you like to find the area and circumference please enter a radius.");
+
+                try
+                {
+                    int input = int.Parse(Console.ReadLine());
+                    Circle circle = new Circle(input);
+                   
+                    Console.WriteLine($"AREA: {circle.CalculateFormatedArea()}");
+                    Console.WriteLine($"CIRCUMFERENCE: {circle.CalculateFormatedCircumference()}\n");
+                    counter++;
+                    go = Continue(counter);
+                    
+                }
+                catch
+                {
+                    Console.WriteLine($"This is invalid please try again.\n");
+                    go = Continue(counter);
+                }
+            }
+        }
+        public static bool Continue(int counter)
+        {
+            Console.WriteLine($"Press {'y'} to continue or any other key to exit. . .\n");
+
+            char response = Console.ReadKey(true).KeyChar;
+
+            if (response == 'y')
+            {
+                Console.Clear();
+                return true;
+            }
+            else if (response != 'y')
+            {
+                Console.WriteLine($"GoodBye! You made {counter} Circles objects");
+                return false;
+            }
+            else
+            {
+                return Continue(counter);
+            }
+        }
+    }
+}
